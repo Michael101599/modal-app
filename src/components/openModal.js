1e15a64b22e4cdf0230f572fe17865634276c1f8
Michael Alvarado <alvaradohernandezmichael@gmail.com>
2022-08-30 22:33:52 -0500

    Modal-app

---
+++ src/components/openModal.js
@@ -0,0 +1,31 @@
+import React, {Suspense, lazy} from "react";
+import {createRoot} from 'react-dom/client'
+import ModalLoading from "./ModalLoading";
+
+export function openModal(){
+    const Modal = lazy(() => import('./ModalSettings'));
+    const modalDiv = document.createElement('div');
+    modalDiv.id = 'modal';
+    document.body.appendChild(modalDiv);
+
+    const root = createRoot(modalDiv);
+    root.render(
+        <Suspense fallback={<ModalLoading/>} >
+            <Modal root={root} title='Modal de configuración'/>
+        </Suspense>
+        )
+}
+
+export function openModalAccount(){
+    const Modal = lazy(() => import('./ModalAccount'));
+    const modalDiv = document.createElement('div');
+    modalDiv.id = 'modal';
+    document.body.appendChild(modalDiv);
+
+    const root = createRoot(modalDiv);
+    root.render(
+        <Suspense fallback={<ModalLoading/>} >
+            <Modal root={root} title='Modal de cuenta'/>
+        </Suspense>
+        )
+}
